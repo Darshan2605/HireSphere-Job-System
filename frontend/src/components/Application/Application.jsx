8ffdf0a16280693917420c4d7e4eb02a0d52589b
Darshan Londhe <116908065+Darshan2605@users.noreply.github.com>
2025-02-23 23:26:14 +0530

    //api updated to /api

--- frontend/src/components/Application/Application.jsx
+++ frontend/src/components/Application/Application.jsx
@@ -35,7 +35,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        `${import.meta.env.VITE_BACKEND_API_URL}/api/v1/application/post`,
+        `${import.meta.env.VITE_BACKEND_API_URL}api/v1/application/post`,
         formData,
         {
           withCredentials: true,
