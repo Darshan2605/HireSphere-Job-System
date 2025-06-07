d13ba48bfb10eecf5f259ac618bb9f19e9a6fd89
Darshan Londhe <116908065+Darshan2605@users.noreply.github.com>
2025-06-07 13:33:28 +0530

    test

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
